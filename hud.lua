35a7fb43fc1b63a5c13c1e90f1e4db7493f70a1d
arocanestor <49110236+arocanestor@users.noreply.github.com>
2020-04-25 10:27:25 -0500

    commit at pelanguero

--- hud.lua
+++ hud.lua
@@ -1,8 +1,6 @@
---[[
-velocidad = 130
-vida = 0 --- 10 opciones desde el 0
+hud={imagen_vida="",vida}
 qy=math.floor(vi*45)+1   
   quad_v = love.graphics.newQuad( 1, qy, 197, 45, 197, 450)
   barra = love.graphics.newImage("barra_v.png")  
 
-  love.graphics.draw( barra,quad_v,1,1 )]]
+  love.graphics.draw( barra,quad_v,1,1 )
