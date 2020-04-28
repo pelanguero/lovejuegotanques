71dde50d490d03e4902f9951da3e59932c4c14ea
arocanestor <49110236+arocanestor@users.noreply.github.com>
2020-04-28 10:12:36 -0500

    mapa

--- hud.lua
+++ hud.lua
@@ -1,4 +1,5 @@
-hud={tilles=nil,tiles="terrainTiles_default.png",smapa={},tablam=nil,barrav="",vida=3,imagen,px=1,py=1,barra="barra_v.png",quads={}}
+hud={anchop,altop,tilles=nil,tiles="terrainTiles_default.png",smapa={},tablam=nil,barrav="",vida=3,imagen,px=1,py=1,barra="barra_v.png",quads={}}
+an,al,tx,ty=nil
 
 function hud.cargarMapa()
    hud.tablam=require("mapadeprueba")
@@ -10,9 +11,17 @@ function hud.cargarMapa()
        a=a+1
        hud.quads[a]=love.graphics.newQuad((j-1)*64,(i-1)*64 , 64, 64, 640, 256)
      end
-   end
- 
+   end 
  end
+function hud.calculapw()
+hud.anchop,hud.altop =  love.window.getMode( )
+hud.anchop = hud.anchop/3
+
+an = (((hud.anchop*2)/100)/64) 
+al = (((hud.altop)/100)/64)
+tx =  64*an
+ty = 64*al
+end
 
 
 hud.imagen=love.graphics.newImage(hud.barra)
@@ -21,16 +30,24 @@ hud.qbarra=love.graphics.newQuad(hud.px,hud.py,197,45,197,450)
 hud.cargarMapa()
 hud.calcularQuads()
 hud.tilless=love.graphics.newImage(hud.tiles)
+hud.calculapw()
+
+function hud.mapa()  
+ 
+end
+
 
 
 function hud.dibujar(x,y)  
    love.graphics.draw(hud.imagen,hud.qbarra,1,1)
-   p=1   
-   for  i=1,100 do 
-      for j=1, 100 do
-        love.graphics.draw(hud.tilless,hud.quads[hud.tablam.layers[1].data[p]],(j-1)*1.28,(i-1)*1.28,0,0.02,0.02)    
-        p=p+1
-      end 
-    end 
+   if love.keyboard.isDown("m") then
+    p=1   
+    for  i=1,100 do 
+       for j=1, 100 do
+         love.graphics.draw(hud.tilless,hud.quads[hud.tablam.layers[1].data[p]],hud.anchop+(j-1)*tx,(i-1)*ty,0,an,al)          
+         p=p+1
+       end 
+     end
+end
 end
 return hud
--- main.lua
+++ main.lua
@@ -15,6 +15,7 @@ ifr=love.graphics.newImage("terrainTiles_default.png")
 xd=0
 yd=0
 end
+hud=require("hud")
 
 function love.update(dt)
     --eventos de mouse para elegir la opcion en el menu
@@ -42,6 +43,7 @@ function love.update(dt)
     elseif love.keyboard.isDown("d") then
         juego.entidades.entidadess[1].angulo=juego.entidades.entidadess[1].angulo+math.rad(100)*dt
     end
+    
 end
 
 function love.draw()
