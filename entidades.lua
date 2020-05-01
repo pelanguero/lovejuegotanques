0ea9c935895b4545233715e6ac0c2631fb90eaf4
arocanestor <49110236+arocanestor@users.noreply.github.com>
2020-04-30 21:56:43 -0500

    map

--- entidades.lua
+++ entidades.lua
@@ -1,12 +1,24 @@
-local entidades={entidadess={}}
+local entidades={entidadess={},ancho=800,alto=600}
 
 function entidades.agregar(ent)
 table.insert(entidades.entidadess,ent)
 end
-function entidades.dibujar(eex,eey)
+function entidades.dibujar(eex,eey,canv)
+  --eex,eey esquina superior izquierda de la camara
   for i=1,#entidades.entidadess do
-      entidades.entidadess[i].dibujar()
+    if entidades.estaDentro(eex,eey,entidades.entidadess[i]) then
+      entidades.entidadess[i].dibujar(eex,eey,canv)
+    end
+        
   end
+  
+end
+function entidades.estaDentro(ax,ay,entidadd)
+  local retorno=false
+  if entidadd.posX-ax<entidades.ancho and entidadd.posY-ay<entidades.alto then
+  retorno=true
+  end
+  return retorno
 end
 
 return entidades
